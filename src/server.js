4396d992611fc9d34165a022c17939e621e72895
paivarobson <paivawork@hotmail.com>
2020-06-04 23:28:53 -0300

    Configuração do servidor Node.js

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+
+// Configurar pasta pública
+server.use(express.static("public"))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+// Configurar caminhos da minha aplicação
+// Página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", function(req, res) {
+    return res.render("index.html", { title: "Um título"}) 
+})
+
+server.get("/create-point", function(req, res) {
+    return res.render("create-point.html") 
+})
+
+
+
+
+// Ligar o servidor
+server.listen(3000)
